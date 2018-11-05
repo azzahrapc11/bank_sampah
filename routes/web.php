42e7473944f54c7bf44de427cf16271407c4d2e4
azzahrapc11 <azzahrapc11>
2018-11-05 22:39:17 +0700

    Baru

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,30 @@
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/dashboard', function () {
+    return view('dashboard');
+});
+Route::get('/notifications', function () {
+    return view('notifications');
+});
+Route::get('/icons', function () {
+    return view('icons');
+});
+Route::get('/map', function () {
+    return view('map');
+});
+Route::get('/tables', function () {
+    return view('tables');
+});
+Route::get('/typography', function () {
+    return view('typography');
+});
+Route::get('/upgrade', function () {
+    return view('upgrade');
+});
+Route::get('/user', function () {
+    return view('user');
+});
 
 Auth::routes();
 
